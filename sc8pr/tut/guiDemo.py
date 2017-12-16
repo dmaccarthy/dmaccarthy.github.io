55b3c83ba09d4d5fcc680e7be4171da3c700511e
D. MacCarthy <sc8pr.py@gmail.com>
2017-12-15 18:22:12 -0700

    Updates

--- sc8pr/tut/guiDemo.py
+++ sc8pr/tut/guiDemo.py
@@ -1,21 +1,36 @@
-from sc8pr import Sketch, TOPLEFT, TOPRIGHT, BOTTOM, CENTER, TOP, Canvas
-from sc8pr.util import rgba
+# Copyright 2017 D.G. MacCarthy <http://dmaccarthy.github.io>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from sc8pr import Sketch, Canvas, Image, TOPLEFT, TOPRIGHT, BOTTOM, CENTER, TOP
+from sc8pr.util import rgba, nothing, sc8prData
 from sc8pr.text import BOLD, Font
 from sc8pr.gui.textinput import TextInput
 from sc8pr.gui.radio import Radio, Options
 from sc8pr.gui.slider import Slider
 from sc8pr.gui.progress import ProgressBar
-from sc8pr.gui.button import Button
+from sc8pr.gui.button import Button, yesNo
+from sc8pr.gui.menu import Menu, R_TRIANGLE
 
 GREY, BLUE = rgba("#ececec", "blue")
 FONT = Font.sans()
 
-def noResize(gr, size): pass
-
 def setup(sk):
     # Create a Canvas as a GUI dialog
     cv = Canvas(sk.size).config(pos=sk.center, bg="#f0f0ff", weight=1)
-    sk += cv.bind(resize=noResize)
+    sk += cv.bind(resize=nothing)
 
     # Add a TextInput
     xc = sk.center[0]
@@ -34,25 +49,35 @@ def setup(sk):
         anchor=BOTTOM, weight=1, change=0.01)
 
     # Add a Radio box
+    cfg = {"font":FONT, "fontSize":14}
     text = "Option A", "Option B", "Option C"
-    radio = Radio(text, font=FONT, fontSize=16).bind(onchange=radioChange)
+    radio = Radio(text, **cfg).bind(onchange=radioChange)
     cv += radio.config(pos=(8, 80), anchor=TOPLEFT, selected=1)
 
     # Add an Options box
     text = "Option X", "Option Y", "Option Z"
-    radio = Options(text, font=FONT, fontSize=16).config(pos=(8, 180), anchor=TOPLEFT)
+    radio = Options(text, **cfg).config(pos=(8, 180), anchor=TOPLEFT)
     cv += radio.bind(onaction=optionsChange)
 
     # Add a selectable button
     size = 120, 48
-    btn = Button(size).content("Selectable\nButton").bind(onaction=buttonClick)
+    btn = Button(size).textIcon("Selectable\nButton", yesNo()[0]).bind(onaction=buttonClick)
     cv += btn.config(pos=(240, 80), anchor=TOP, name="Selectable")
-    btn[0].config(font=FONT, fontSize=16, color=BLUE, align=CENTER)
+    btn[-1].config(color=BLUE, align=CENTER, **cfg)
 
-    # Add a non-selectable 'Okay' button
-    btn = Button(size, 2).content("Okay").bind(onaction=buttonClick)
-    cv += btn.config(pos=(240, 180), anchor=TOP, name="Okay")
-    btn[0].config(font=FONT, fontSize=16)
+    # Add a non-selectable button
+    btn = Button(size, 2).textIcon("Popup Menu").bind(onaction=buttonClick)
+    btn[-1].config(**cfg)
+    cv += btn.config(pos=(240, 180), anchor=TOP, name="Popup")
+
+    # Create a popup menu
+    img = Image.fromBytes(sc8prData("alien"))
+    items = [("Action", img, None), ("Back", None, R_TRIANGLE)]
+    cv.menu = Menu(items, txtConfig=cfg).config(pos=sk.center)
+    cv.menu.bind(resize=nothing, onaction=menuAction)
+
+    # Create a cover to block other controls when popup running
+    cv.cover = Image(bg="#ffffffc0").config(anchor=TOPLEFT)
 
 def onaction(gr, ev):
     "Event handler for TextInput"
@@ -61,6 +86,16 @@ def onaction(gr, ev):
 def radioChange(gr, ev):
     "Event handler for Radio"
     print(gr, gr.selected.layer)
+
+def menuAction(menu, ev):
+    "Event handler for Menu"
+    n = menu.buttonNumber(ev.target)
+    print(menu, n)
+    if n == 1: # Item 1 = Back
+        for btn in menu: btn.status = "normal"
+        cv = menu.canvas
+        cv.cover.remove()
+        menu.remove()
 
 def optionsChange(gr, ev):
     "Event handler for Options"
@@ -74,6 +109,11 @@ def sliderChange(gr, ev):
 def buttonClick(btn, ev):
     "Event handler for buttons"
     print(btn, btn.selected)
+    if btn.name == "Popup":
+        cv = btn.canvas
+        btn.status = "normal"
+        cv += cv.cover.config(size=cv.size)
+        cv += cv.menu
 
 def progress(gr):
     "Update progress bar"
