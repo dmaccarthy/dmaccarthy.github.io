f3d273d17bd6fce08c34dd1a0f1a4c5a293870c4
D. MacCarthy <sc8pr.py@gmail.com>
2018-04-12 15:58:43 -0600

    More Pages

--- sc8pr/sitemap.js
+++ sc8pr/sitemap.js
@@ -27,11 +27,11 @@ var sitemap = {title:"sc8pr 2.0 Home", link:"home", pages:[
 		{title:"sc8pr.geom", link:"geom"},
 		{title:"sc8pr.gui.*", link:"gui", pages:[
 			{title:"class Button", link:"button_class"},
-//			{title:"class Menu", link:""},
+			{title:"class Menu", link:"menu_class"},
 //			{title:"class Options", link:"options_class"},
 //			{title:"class Radio", link:""},
 //			{title:"class Slider", link:""},
-//			{title:"class TextInput", link:"ti_class"},
+			{title:"class TextInput", link:"ti_class"}
 		]},
 		{title:"sc8pr.misc.*", link:"misc", pages:[
 			{title:".effect", link:"effect"},
