13f923e7b4752987c1ab6d2ee34d926bb9be2dce
D. MacCarthy <sc8pr.py@gmail.com>
2020-05-23 18:27:23 -0600

    Update plotcanvasDemo.py

--- sc8pr/misc/plotcanvasDemo.py
+++ sc8pr/misc/plotcanvasDemo.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 """
 A demonstration of plotting and scrolling canvases using sc8pr.plot*
 (*Experimental package in sc8pr v2.2)
@@ -26,14 +25,62 @@ from sc8pr.plot import PSketch, PCanvas
 from sc8pr.plot.shape import PLocus, PVector
 from sc8pr.text import Font, ITALIC
 from sc8pr.shape import Circle
-from sc8pr.util import ondrag
+from sc8pr.util import mix, ondrag
+from sc8pr.misc.plot import expon, leastSq
+
+def setup(sk):
+    "Draw a graph directly onto the PSketch"
+    
+    # Allow free resize with no scaling of graphics
+    sk.config(fixedAspect=False, resizeContent=False)
+
+    # Draw the coordinate grid from 0 to 10 using default step of 1
+    grid = [0, 10, 0, 10]
+    sk.gridlines(grid)
+
+    # Draw some bars
+    data = (2, 3.6), (4, 3), (6, 1.8), (8, 6)
+    sk += sk.series(data, fill="yellow")
+
+    # Draw the x and y axes
+    sk.axis(grid)
+
+    # Graph some data points; use zip function if x and y data are separate
+    x, y = (0, 3, 6, 9), (0.5, 1, 4, 6)
+    for dot in sk.series(zip(x, y), Circle(50).config(fill="red")):
+        sk += dot.config(height=10)
+
+    # Exponential and linear regressions
+    sk += PLocus(expon(x, y)[0], (0, 10)).config(weight=2, stroke="darkgreen")
+    sk += PLocus(leastSq(x, y)[0], (0, 10)).config(weight=2)
+
+    # Label the data with some adjacent text
+    font = {"font": Font.serif(), "fontSize": 16}
+    sk += sk.series(zip(x, y), list("ABCD"), (0.2, 0.2), color="red", **font)
+
+    # Label the axes
+    sk += sk.series([(10, 0), (0, 10)], ["x", "y"], anchor=TOPRIGHT, color="blue", fontStyle=ITALIC, **font)
+    sk += sk.series(mix(range(0, 9, 2), -0.3), "{x}", **font)
+    sk += sk.series(mix(-0.2, range(0, 9, 2)), "{y}", **font)
+
+    # Animated wave
+    sk += PLocus(wave, (0, 10), A=2, k=3, x0=6, T=1, sk=sk).config(weight=2, stroke="purple")
+
+    # Draw an inset vector diagram
+    sk += vecDiag().config(weight=1, pos=sk.px(3, 7.5)).bind(ondrag)
+
+
+def wave(x, A, k, x0, T, sk):
+    "Animated sine-wave function"
+    phase = 2 * pi / T * sk.frameCount / sk.frameRate
+    return A * sin(k * x - phase) + x0
 
 def vecDiag():
-    "Draw a vector diagram"
+    "Draw a vector diagram in a PCanvas"
 
-    # Create a plotting canvas
+    # Create a plotting canvas with grid lines and axis
     cv = PCanvas(240, [-2.5, 6.5, -2.5, 4.5], bg="#fcfcfc")
-    cv.gridlines([-2, 6, -2, 4], [1, 1], {})
+    cv.gridlines([-2, 6, -2, 4], 1, {})
 
     # Create a vector
     v = PVector(6, 35).config(stroke="blue")
@@ -42,42 +89,5 @@ def vecDiag():
     cv += v.components() + [v]
     return cv
 
-def setup(sk):
-    "Draw a graph directly onto the sketch"
 
-    # Draw coordinate grid
-    sk.config(resizeContent=False, fixedAspect=False)
-    grid = [0, 10, 0, 10]
-    sk.gridlines(grid, (1, 1))
-
-    # Label axes
-    font = {"font": Font.serif(), "fontSize": 16}
-    sk.graph([(10, 0), (0, 10)], ["x", "y"], anchor=TOPRIGHT, color="blue", fontStyle=ITALIC, **font)
-    sk.mix(range(0, 9, 2), -0.3, "{x}", **font)
-    sk.mix(-0.2, range(0, 9, 2), "{y}", **font)
-
-    # Draw some bars and the axes
-    data = (2, 3.6), (4, 3), (6, -0.2), (8, 6)
-    sk.graph(data, 0.75, fill="yellow")
-    sk[-2].config(fill="red")
-    sk.axis(grid)
-
-    # Draw some points and labels
-    data = (0, 0), (3, 1), (6, 3), (9, 6)
-    sk.graph(data, Circle(50).config(fill="red"))
-    for gr in sk[-len(data):]: gr.config(height=10)
-    sk.graph(data, list("ABCD"), (0.2, 0.2), color="red", **font)
-
-    # Graph an equation
-    sk += PLocus(wave, [0, 10], A=2, k=3, x0=6, T=1, sk=sk).config(weight=2, stroke="purple")
-
-    # Draw an inset vector diagram
-    vecDiag().setCanvas(sk).config(weight=1, csPos=(3, 7.5)).bind(ondrag)
-
-def wave(x, A, k, x0, T, sk):
-    "Animated sine-wave function"
-    phase = 2 * pi * sk.frameCount / (T * sk.frameRate)
-    return A * sin(k * x - phase) + x0
-
-
-PSketch((512, 288), [-0.75, 10.25, -0.75], (640, 640)).bind().play("Plotting & Scrolling Demo")
+PSketch((512, 288), [-0.75, 10.25, -0.75], (640, 640)).play("Plotting & Scrolling Demo")
